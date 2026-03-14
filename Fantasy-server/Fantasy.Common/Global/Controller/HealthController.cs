8c8b8238c60b5bfe6448a618ba20db251028e91b
PARK HYEONMIN (박현민) <b01080080329@gmail.com>
2026-03-14 23:46:13 +0900

    feat: health 체크 컨트롤러 추가

---
+++ Fantasy-server/Fantasy.Common/Global/Controller/HealthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fantasy.Common.Global.Controller;
+
+[ApiController]
+[Route("v1/health")]
+[Tags("Health")]
+public class HealthController : ControllerBase 
+{
+    /// <summary>서버 상태 확인</summary>
+    /// <remarks>서버가 정상적으로 작동하는지 확인합니다.</remarks>
+    /// <response code="200">서버 정상</response>
+    [HttpGet]
+    public IActionResult CheckHealth()
+    {
+        return Ok(new
+        {
+            status = "healthy",
+            timestamp = DateTime.UtcNow
+        });
+    }
+}
