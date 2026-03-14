66cc335d835c4a777ae4dc020db43c8a250850db
PARK HYEONMIN (박현민) <b01080080329@gmail.com>
2026-03-14 23:19:07 +0900

    feat: db 연결 config 클래스 추가

---
+++ Fantasy-server/Fantasy.Common/Global/Config/DatabaseConfig.cs
@@ -0,0 +1,20 @@
+﻿using Fantasy.Common.Global.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fantasy.Common.Global.Config;
+
+public static class DatabaseConfig
+{
+    public static IServiceCollection AddDatabase(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(
+                configuration.GetConnectionString("Database")));
+
+        return services;
+    }
+}
