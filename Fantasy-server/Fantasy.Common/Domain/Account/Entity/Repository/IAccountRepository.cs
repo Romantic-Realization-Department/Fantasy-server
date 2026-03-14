eacdf114281eebc7177be0daea07bf51405ce58f
PARK HYEONMIN (박현민) <b01080080329@gmail.com>
2026-03-15 00:42:27 +0900

    feat: IAccountRepository 추가

---
+++ Fantasy-server/Fantasy.Common/Domain/Account/Entity/Repository/IAccountRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Fantasy.Common.Domain.Account.Entity.Repository;
+
+public interface IAccountRepository
+{
+    Task<Account?> FindByEmailAsync(string email);
+    Task<Account> SaveAsync(Account account);
+    Task<bool> ExistsByEmailAsync(string email);
+    Task DeleteAsync(Account account);
+}
