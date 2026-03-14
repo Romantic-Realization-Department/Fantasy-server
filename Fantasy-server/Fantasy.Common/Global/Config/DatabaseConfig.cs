5aa25c796590b82cf79d2403d0b17d4e4b4ccfa9
PARK HYEONMIN (박현민) <b01080080329@gmail.com>
2026-03-14 23:24:12 +0900

    feat: database/cache config 추가

--- Fantasy-server/Fantasy.Common/Global/Config/DatabaseConfig.cs
+++ Fantasy-server/Fantasy.Common/Global/Config/DatabaseConfig.cs
@@ -11,9 +11,12 @@ public static class DatabaseConfig
         this IServiceCollection services,
         IConfiguration configuration)
     {
+        var connectionString = configuration.GetConnectionString("Database")
+            ??  throw new InvalidOperationException("Database connection string is missing.");
+        
         services.AddDbContext<AppDbContext>(options =>
             options.UseNpgsql(
-                configuration.GetConnectionString("Database")));
+                configuration.GetConnectionString(connectionString)));
 
         return services;
     }
