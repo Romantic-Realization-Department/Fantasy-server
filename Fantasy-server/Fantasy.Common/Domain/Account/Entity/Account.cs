25297de8fb034e304a33a4ec04d03d4b864a681c
PARK HYEONMIN (박현민) <b01080080329@gmail.com>
2026-03-15 00:34:41 +0900

    feat: 계정 DB 모뎅 추가

---
+++ Fantasy-server/Fantasy.Common/Domain/Account/Entity/Account.cs
@@ -0,0 +1,22 @@
+﻿using Fantasy.Common.Domain.Account.Entity.Constant;
+
+namespace Fantasy.Common.Domain.Account.Entity;
+
+public class Account
+{
+    public long Id { get; private set; }
+    public string Email { get; private set; } = string.Empty;
+    public string Password { get; private set; }= string.Empty;
+    public AccountRole Role { get; private set; }
+    public bool IsNewAccount { get; private set; } = true;
+    public DateTime CreatedAt { get; private set; }
+    public DateTime UpdatedAt { get; private set; }
+    
+    public static Account Create(string email) => new()
+    {
+        Email = email,
+        IsNewAccount = true,
+        CreatedAt = DateTime.UtcNow,
+        UpdatedAt = DateTime.UtcNow
+    };
+}
