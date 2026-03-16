af998147ef0a8d3867e8168896898acd4aef1d7c
Sean-mn <b01080080329@gmail.com>
2026-03-17 08:16:12 +0900

    fix: 누락된 DI 등록 및 개발 환경 설정 추가

--- Fantasy-server/Fantasy.Auth/Program.cs
+++ Fantasy-server/Fantasy.Auth/Program.cs
@@ -1,6 +1,10 @@
 using Fantasy.Auth.Domain.Account.Service;
 using Fantasy.Auth.Domain.Account.Service.Interface;
+using Fantasy.Auth.Domain.Auth.Repository;
+using Fantasy.Auth.Domain.Auth.Service;
+using Fantasy.Auth.Domain.Auth.Service.Interface;
 using Fantasy.Auth.Global.Security.Provider;
+using Fantasy.Common.Domain.Auth.Repository;
 using Fantasy.Common.Global.Config;
 using Fantasy.Common.Global.Security.Filter;
 
@@ -8,16 +12,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddDatabase(builder.Configuration);
+builder.Services.AddRedis(builder.Configuration, "auth:");
 builder.Services.AddJwtAuthentication(builder.Configuration);
+builder.Services.AddRateLimit();
 
 builder.Services.AddScoped<ICreateAccountService, CreateAccountService>();
 builder.Services.AddScoped<IDeleteAccountService, DeleteAccountService>();
+builder.Services.AddScoped<ILoginService, LoginService>();
+builder.Services.AddScoped<IRefreshTokenRedisRepository, RefreshTokenRedisRepository>();
 
-builder.Services.AddScoped<JwtProvider>();
-builder.Services.AddScoped<JwtAuthenticationFilter>();
+builder.Services.AddSingleton<JwtProvider>();
+builder.Services.AddSingleton<JwtAuthenticationFilter>();
 
 var app = builder.Build();
 
+app.UseRateLimiter();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
