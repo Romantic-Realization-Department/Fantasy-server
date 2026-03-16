451af5f2c22aaa54b369b1d4f07f69abf9103595
Sean-mn <b01080080329@gmail.com>
2026-03-16 09:03:48 +0900

    feat: Auth 서비스 DI 및 미들웨어 등록

--- Fantasy-server/Fantasy.Auth/Program.cs
+++ Fantasy-server/Fantasy.Auth/Program.cs
@@ -1,23 +1,24 @@
+using Fantasy.Auth.Domain.Account.Service;
+using Fantasy.Auth.Domain.Account.Service.Interface;
+using Fantasy.Auth.Global.Security.Provider;
+using Fantasy.Common.Global.Config;
+using Fantasy.Common.Global.Security.Filter;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddJwtAuthentication(builder.Configuration);
+
+builder.Services.AddScoped<ICreateAccountService, CreateAccountService>();
+builder.Services.AddScoped<IDeleteAccountService, DeleteAccountService>();
+
+builder.Services.AddScoped<JwtProvider>();
+builder.Services.AddScoped<JwtAuthenticationFilter>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
