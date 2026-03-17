e7198d97ab2bf89c56a678af9f0d79cc39e1c946
Sean-mn <b01080080329@gmail.com>
2026-03-17 11:04:57 +0900

    feat: DeleteAccountService 구현

--- Fantasy-server/Fantasy.Auth/Domain/Account/Service/DeleteAccountService.cs
+++ Fantasy-server/Fantasy.Auth/Domain/Account/Service/DeleteAccountService.cs
@@ -1,12 +1,37 @@
-﻿using Fantasy.Auth.Domain.Account.Service.Interface;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Fantasy.Auth.Domain.Account.Service.Interface;
+using Fantasy.Auth.Global.Security.Provider;
 using Fantasy.Common.Domain.Account.Dto.Request;
+using Fantasy.Common.Domain.Account.Repository;
+using Fantasy.Common.Domain.Auth.Exception;
+using Microsoft.AspNetCore.Http;
 
 namespace Fantasy.Auth.Domain.Account.Service;
 
 public class DeleteAccountService : IDeleteAccountService
 {
-    public Task ExecuteAsync(DeleteAccountRequest request)
+    private readonly IAccountRepository _accountRepository;
+    private readonly ICurrentUserProvider _currentUserProvider;
+
+    public DeleteAccountService(
+        IAccountRepository accountRepository,
+        ICurrentUserProvider currentUserProvider)
     {
-        throw new NotImplementedException();
+        _accountRepository = accountRepository;
+        _currentUserProvider = currentUserProvider;
+    }
+
+    public async Task ExecuteAsync(DeleteAccountRequest request)
+    {
+        var email = _currentUserProvider.GetEmail();
+        
+        var account = await _accountRepository.FindByEmailAsync(email)
+            ?? throw new InvalidCredentialsException();
+
+        if (!BCrypt.Net.BCrypt.Verify(request.Password, account.Password))
+            throw new InvalidCredentialsException();
+
+        await _accountRepository.DeleteAsync(account);
     }
 }
