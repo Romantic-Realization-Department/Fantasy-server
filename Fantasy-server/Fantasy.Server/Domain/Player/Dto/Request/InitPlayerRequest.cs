be655711720285c459e3e438d43dc280feb1e6a1
Sean-mn <b01080080329@gmail.com>
2026-06-29 11:10:37 +0900

    refactor: player init을 GET 로드 + POST 생성으로 분리

--- Fantasy-server/Fantasy.Server/Domain/Player/Dto/Request/InitPlayerRequest.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-using Fantasy.Server.Domain.Player.Enum;
-
-namespace Fantasy.Server.Domain.Player.Dto.Request;
-
-public record InitPlayerRequest(
-    [Required] JobType JobType
-);
