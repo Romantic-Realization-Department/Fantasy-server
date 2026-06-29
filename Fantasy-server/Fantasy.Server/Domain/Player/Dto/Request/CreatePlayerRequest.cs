77e9a46bda46fb7b1e2eefe8c95e0b47e474ab83
Sean-mn <b01080080329@gmail.com>
2026-06-29 15:13:25 +0900

    fix: CreatePlayerRequest의 JobType에 EnumDataType 유효성 검사 추가
    [Required]만으로는 미정의 정수 값(예: 99)이 모델 검증을 통과해
    도메인 영역으로 유입될 수 있어, [EnumDataType(typeof(JobType))]으로
    정의된 JobType 값만 허용하도록 보강.

--- Fantasy-server/Fantasy.Server/Domain/Player/Dto/Request/CreatePlayerRequest.cs
+++ Fantasy-server/Fantasy.Server/Domain/Player/Dto/Request/CreatePlayerRequest.cs
@@ -4,5 +4,5 @@ using Fantasy.Server.Domain.Player.Enum;
 namespace Fantasy.Server.Domain.Player.Dto.Request;
 
 public record CreatePlayerRequest(
-    [Required] JobType JobType
+    [Required][EnumDataType(typeof(JobType))] JobType JobType
 );
